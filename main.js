56bcfb561427f803f83f114dad381b2f58c75f79
seunwanHam <seungwan7511@gmail.com>
2021-11-29 17:22:51 +0900

    11.29 최종 수정

--- main.js
+++ main.js
@@ -124,8 +124,8 @@ uploadBox.addEventListener('drop', function(e) {
 
   newImage.src = URL.createObjectURL(data);   
 
-  newImage.style.width = "30px";
-  newImage.style.height = "30px";
+  // newImage.style.width = "30px";
+  // newImage.style.height = "30px";
   newImage.style.visibility = "hidden";   //버튼을 누르기 전까지는 이미지 숨기기
   // newImage.style.objectFit = "contain";
 
@@ -145,10 +145,10 @@ function loadFile(input) {
 
   newImage.src = URL.createObjectURL(file);   
 
-  newImage.style.width = "70%";
-  newImage.style.height = "70%";
+  // newImage.style.width = "70%";
+  // newImage.style.height = "70%";
   newImage.style.visibility = "hidden";   //버튼을 누르기 전까지는 이미지 숨기기
-  newImage.style.objectFit = "contain";
+  // newImage.style.objectFit = "contain";
 
   let container = document.getElementById('image-show');
   container.appendChild(newImage);
