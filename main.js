354d20abbfed8e269b634654a3e405f2564568da
seunwanHam <seungwan7511@gmail.com>
2021-11-29 17:26:36 +0900

    11.29 skipnavi 추가

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// Make navbar transparent when it is on the top
+// navbar
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
@@ -11,7 +11,7 @@ document.addEventListener('scroll', () => {
   }
 });
 
-// Handle scrolling when tapping on the navbar menu
+// navbar 메뉴 클릭할 때 스크롤 처리
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
   const target = event.target;
@@ -23,26 +23,25 @@ navbarMenu.addEventListener('click', (event) => {
   scrollIntoView(link);
 });
 
-// Navbar toggle button for small screen
+// 작은 화면에서의 navbar toggle 버튼
 const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
 navbarToggleBtn.addEventListener('click', () => {
   navbarMenu.classList.toggle('open');
 });
 
-// Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click', () => {
   scrollIntoView('#footer');
 });
 
-// Make home fade to transparent as the window scrolls down
+// 스크롤 내릴 시 fadeout 처리
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
-// show "arrow up" button when scrolling down
+// 아래로 스크롤 할 때 "arrow up" 버튼 표시
 const arrowUp = document.querySelector('.arrow-up');
 document.addEventListener('scroll', ()=> {
   if(window.scrollY > homeHeight / 2) {
@@ -52,7 +51,7 @@ document.addEventListener('scroll', ()=> {
   }
 });
 
-// Handle click on the "arrow up" button
+// "arrow up" 버튼 클릭 처리
 arrowUp.addEventListener('click', ()=> {
   scrollIntoView('#home');
 });
@@ -65,9 +64,7 @@ function scrollIntoView(selector) {
 function showImage() {
   let newImage = document.getElementById('image-show').lastElementChild;
   newImage.style.visibility = "visible";
-  // let ellipes_download = document.querySelector('.ellipes_download');
   let final_download = document.querySelector('.final_download');
-  // ellipes_download.style.visibility = "visible";
   final_download.style.visibility = "visible";
 
   document.getElementById('image-upload').style.visibility = 'hidden';
@@ -114,8 +111,6 @@ uploadBox.addEventListener('drop', function(e) {
   let data = e.dataTransfer.files[0];
   console.dir(data);
 
-  // let file = input.files[0];
-
   let name = document.getElementById('fileName');
   name.textContent = data.name;
 
@@ -123,11 +118,7 @@ uploadBox.addEventListener('drop', function(e) {
   newImage.setAttribute("class", 'img');
 
   newImage.src = URL.createObjectURL(data);   
-
-  // newImage.style.width = "30px";
-  // newImage.style.height = "30px";
   newImage.style.visibility = "hidden";   //버튼을 누르기 전까지는 이미지 숨기기
-  // newImage.style.objectFit = "contain";
 
   let container = document.getElementById('image-show');
   container.appendChild(newImage);  
@@ -145,10 +136,7 @@ function loadFile(input) {
 
   newImage.src = URL.createObjectURL(file);   
 
-  // newImage.style.width = "70%";
-  // newImage.style.height = "70%";
   newImage.style.visibility = "hidden";   //버튼을 누르기 전까지는 이미지 숨기기
-  // newImage.style.objectFit = "contain";
 
   let container = document.getElementById('image-show');
   container.appendChild(newImage);
